8d997748459cf59a4271e57cd4ada01898a3c76e
Andrew Tae <andrewtae3368@gmail.com>
2017-01-05 22:58:17 -0800

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,56 @@
+
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+
+const zenImage = require('./assets/zen21-1.png')
+
+export default class RelaxationStation extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={() => {alert('I was pressed!')}}>
+          <Image style={styles.buttonImage} source={zenImage} />
+        </TouchableOpacity>
+        <Text style={styles.readyText}> I'm ready to relax...</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#161737',
+  },
+  readyText: {
+    fontSize:20,
+    fontStyle: 'italic',
+    color: '#ffffff',
+  },
+  button: {
+    backgroundColor: '#859a9b',
+    borderRadius: 20,
+    padding: 10,
+    marginBottom: 20,
+    shadowColor: '#303838',
+    shadowOffset: { width: 0, height: 5},
+    shadowRadius: 10,
+    shadowOpacity: 0.35,
+  },
+  buttonImage: {
+    width: 200,
+    height: 200,
+  }
+});
+
+AppRegistry.registerComponent('RelaxationStation', () => RelaxationStation);
